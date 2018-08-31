563b328cdd2033741415cc3918ed153389a2c816
liyuwei <yuwei770@qq.com>
2018-09-01 01:04:49 +0800

    完成了新增操作

--- src/controllers/studentmanagerController.js
+++ src/controllers/studentmanagerController.js
@@ -23,3 +23,28 @@ exports.getStudentListPage = (req, res) => {
     })
 
 }
+/*
+* 最终处理，返回获取到的学生列表页面
+*/
+exports.getAddStudentPage  = (req, res) => {
+    xtpl.renderFile(path.join(__dirname, "../statics/views/add.html"), {
+       
+    }, function (error, content) {
+        res.send(content)
+    });
+}
+
+/*
+*  最终处理，返回新增操作之后的html(html中有一段可以执行js)
+*/
+exports.addStudent =(req,res)=>{
+    // console.log(req.body);
+    databasetool.insertOne("studentInfo", req.body, (err,result)=>{
+        if(result ==null){
+            //表示新增失败
+            res.send(`<script>alert("新增失败!");</script>`);
+        }else{
+            res.send(`<script>window.location.href='/studentmanager/list'</script>`)
+        }
+    })
+}
--- src/routers/studentmanagerRouter.js
+++ src/routers/studentmanagerRouter.js
@@ -6,8 +6,12 @@ const studentmanagerRouter = express.Router()
 const studentmanagerCTRL = require(path.join(__dirname, "../controllers/studentmanagerController.js"))
 
 //处理具体请求
+//获得学生列表页
 studentmanagerRouter.get('/list', studentmanagerCTRL.getStudentListPage)
-
+//获得新增页面
+studentmanagerRouter.get('/add',studentmanagerCTRL.getAddStudentPage )
+//进行新增操作
+studentmanagerRouter.post('/add',studentmanagerCTRL.addStudent)
 
 
 
