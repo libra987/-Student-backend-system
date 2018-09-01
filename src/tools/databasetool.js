46a0325fb5485618a3ebe0bd4a2f3b1f29d1e7ef
liyuwei <yuwei770@qq.com>
2018-09-01 08:53:24 +0800

    对databasetool中连接数据库操作的封装

--- src/tools/databasetool.js
+++ src/tools/databasetool.js
@@ -1,13 +1,30 @@
 //连接数据库
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require("mongodb").ObjectId
+//导出
 exports.ObjectId = ObjectId
+
 // Connection URL
 const url = 'mongodb://localhost:27017';
 
 // Database Name
 const dbName = 'szhmqd21';
 
+
+const connectDB = (collectionName, callback) => {
+    MongoClient.connect(
+        url,
+        { useNewUrlParser: true },
+        function (err, client) {
+            // 拿到了数据操作的db对象
+            const db = client.db(dbName);
+            // 拿到集合
+            const collection = db.collection(collectionName);
+            // 把结果传递出去
+            callback(err, client, collection);
+        }
+    );
+};
 
 
 /**
@@ -17,21 +34,16 @@ const dbName = 'szhmqd21';
  * @param {*} callback 回调函数
  */
 exports.findList = (collectionName, params, callback) => {
-    // 
-    MongoClient.connect(
-        url,
-        { useNewUrlParser: true },
-        function (err, client) {
-            //拿到db对象
-            const db = client.db(dbName);
-            //拿到合集
-            const collection = db.collection(collectionName);
 
-            collection.find(params).toArray(function (err, docs) {
-                client.close();
-                callback(err, docs)
-            });
+    connectDB(collectionName, (err, client, collection) => {
+        //连接数据库,根据条件查询
+        collection.find(params).toArray(function (err, docs) {
+            client.close();
+            // 执行 callback 把结果返回给控制器
+            callback(err, docs)
         });
+    })
+
 }
 
 /**
@@ -41,20 +53,14 @@ exports.findList = (collectionName, params, callback) => {
  * @param {*} callback 回调函数
  */
 exports.findOne = (collectionName, params, callback) => {
-    MongoClient.connect(
-        url,
-        { useNewUrlParser: true },
-        function (err, client) {
-            //拿到db对象
-            const db = client.db(dbName);
-            //拿到合集
-            const collection = db.collection(collectionName);
 
-            collection.findOne(params, (err, doc) => {
-                client.close();
-                callback(err, doc)
-            });
+    connectDB(collectionName, (err, client, collection) => {
+        collection.findOne(params, (err, doc) => {
+            client.close();
+            callback(err, doc)
         });
+    })
+
 }
 
 /**
@@ -64,20 +70,14 @@ exports.findOne = (collectionName, params, callback) => {
  * @param {*} callback 回调函数
  */
 exports.insertOne = (collectionName, params, callback) => {
-    MongoClient.connect(
-        url,
-        { useNewUrlParser: true },
-        function (err, client) {
-            //拿到db对象
-            const db = client.db(dbName);
-            //拿到合集
-            const collection = db.collection(collectionName);
 
-            collection.insertOne(params, (err, result) => {
-                client.close();
-                callback(err, result)
-            });
+    connectDB(collectionName, (err, client, collection) => {
+        //连接数据库,根据条件查询
+        collection.insertOne(params, (err, result) => {
+            client.close();
+            callback(err, result)
         });
+    })
 }
 
 /**
@@ -87,19 +87,12 @@ exports.insertOne = (collectionName, params, callback) => {
  * @param {*} params 参数对象
  * @param {*} callback 回调函数
  */
-exports.updateOne =(collectionName,condition, params, callback)=>{
-    MongoClient.connect(
-        url,
-        { useNewUrlParser: true },
-        function (err, client) {
-            //拿到db对象
-            const db = client.db(dbName);
-            //拿到合集
-            const collection = db.collection(collectionName);
-
-            collection.updateOne(condition,{$set:params},(err, result) => {
-                client.close();
-                callback(err, result)
-            });
+exports.updateOne = (collectionName, condition, params, callback) => {
+    connectDB(collectionName, (err, client, collection) => {
+        //连接数据库,根据条件查询
+        collection.updateOne(condition, { $set: params }, (err, result) => {
+            client.close();
+            callback(err, result)
         });
+    })
 }
